c986d1a0930470477ec9ffd75f0b1bf410e2ac8a
Luca <lucahhaller@gmail.com>
2024-05-06 12:45:31 -0300

    cambiosafip

--- client/src/Pages/Afip/Content.jsx
+++ client/src/Pages/Afip/Content.jsx
@@ -50,9 +50,9 @@ export default function Content() {
                   <br className="hidden md:block" />
                 </h2>
                 <p className="text-base  xs:text-justify lg:text-left  text-gray-700 md:text-lg">
-                  Se realizó la construcción del edificio principal de AFIP en
-                  la ciudad de San Luis, junto con los baños y comedor
-                  principales dentro dicho departamento.
+                  Se realizó la remodelación y mantenimiento del edificio
+                  principal de AFIP en la ciudad de San Luis, junto con los
+                  baños y comedor principales dentro dicho departamento.
                 </p>
                 <div className=" flex  cursor-pointer">
                   <div className="relative inline-flex items-center justify-start py-3  pr-12 overflow-hidden font-semibold  text-black-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6   dark:text-white dark:hover:text-gray-200 dark:shadow-none group">
